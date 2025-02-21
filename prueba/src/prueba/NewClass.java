16ec4a47cc4c9c61def86f5fe56e88b360c5bf5e
Anonimohn20 <anonimohn20@gmail.com>
2025-02-20 20:16:12 -0600

    clase nueva

---
+++ prueba/src/prueba/NewClass.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba;
+
+/**
+ *
+ * @author david
+ */
+public class NewClass {
+    int edad;
+    String nombre;
+
+    public NewClass(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "NewClass{" + "edad=" + edad + ", nombre=" + nombre + '}';
+    }
+    
+}
